de1da54f402a59eea5086c669955f278022a96c7
Vinxentxx <vinx220203@gmail.com>
2024-10-21 12:46:32 +0700

    s

--- fend/carousel/record.php
+++ fend/carousel/record.php
@@ -10,11 +10,11 @@ foreach($_SESSION['sid'] as $pid) {
 }
 
 // ตรวจสอบค่าของ member_id และ cr_id
-$cr_id = isset($_SESSION['cr_id']) ? $_SESSION['cr_id'] : null; 
+//$member_id = isset($_SESSION['member_id']) ? $_SESSION['member_id'] : null; 
 $cr_id = isset($_SESSION['cr_id']) ? $_SESSION['cr_id'] : null; 
 
 // สร้างคำสั่ง SQL
-$sql = "INSERT INTO `orders` (odate, ototal, cr_id, cr_id) VALUES (CURRENT_TIMESTAMP, '$total', '$cr_id', ". ($cr_id ? "'$cr_id'" : "NULL") .");";
+$sql = "INSERT INTO `orders` (odate, ototal,  cr_id) VALUES (CURRENT_TIMESTAMP, '$total', ". ($cr_id ? "'$cr_id'" : "NULL") .");";
 mysqli_query($conn, $sql) or die ("insert error: " . mysqli_error($conn));
 $id = mysqli_insert_id($conn);
 
