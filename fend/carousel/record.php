920e0ae52563fd4ae4fb2503d2cc51920f072097
Vinxentxx <vinx220203@gmail.com>
2024-10-21 12:53:41 +0700

    s

--- fend/carousel/record.php
+++ fend/carousel/record.php
@@ -1,33 +1,32 @@
 <?php
-@session_start();
+session_start();
 include("connectdb.php");
+
+// ตรวจสอบให้แน่ใจว่าผู้ใช้ได้เข้าสู่ระบบ
+if (!isset($_SESSION['cr_id'])) {
+    header("Location: login.php"); // เปลี่ยนเส้นทางไปยังหน้าลงชื่อเข้าใช้
+    exit();
+}
 
 $total = 0; // กำหนดตัวแปรรวมราคา
 
-foreach($_SESSION['sid'] as $pid) {
+foreach ($_SESSION['sid'] as $pid) {
     $sum[$pid] = $_SESSION['sprice'][$pid] * $_SESSION['sitem'][$pid];
     $total += $sum[$pid];
 }
 
-// ตรวจสอบค่าของ cr_id
-$cr_id = isset($_SESSION['cr_id']) ? $_SESSION['cr_id'] : null;
-
-// ตรวจสอบให้แน่ใจว่ามีค่า cr_id
-if (is_null($cr_id)) {
-    die("Error: cr_id cannot be null. Please ensure that the user is logged in.");
-}
+// รับค่า cr_id จากเซสชัน
+$cr_id = $_SESSION['cr_id'];
 
 // สร้างคำสั่ง SQL
 $sql = "INSERT INTO `orders` (odate, ototal, cr_id) VALUES (CURRENT_TIMESTAMP, '$total', '$cr_id');";
-mysqli_query($conn, $sql) or die ("insert error: " . mysqli_error($conn));
+mysqli_query($conn, $sql) or die("insert error: " . mysqli_error($conn));
 $id = mysqli_insert_id($conn);
 
-foreach($_SESSION['sid'] as $pid) {
+foreach ($_SESSION['sid'] as $pid) {
     $sql2 = "INSERT INTO `orders_detail` (oid, pid, item) VALUES ('$id', '".$_SESSION['sid'][$pid]."', '".$_SESSION['sitem'][$pid]."');";
-    mysqli_query($conn, $sql2) or die ("insert detail error: " . mysqli_error($conn));
+    mysqli_query($conn, $sql2) or die("insert detail error: " . mysqli_error($conn));
 }
 
 echo "<meta http-equiv=\"refresh\" content=\"0;URL=order.php\">";
 ?>
-    ini_set('display_errors', 1);
-    error_reporting(E_ALL);
